5518137d313bc9bc4df2e67297d1237cfd062c74
DeyviCano <mcano@nogasa.com.pe>
2026-05-13 12:13:47 -0500

    Correccion de importacion de de CircularProgress

--- app/src/main/java/com/vistony/app/Screen/Inspeccion/HomeInspeccion.kt
+++ app/src/main/java/com/vistony/app/Screen/Inspeccion/HomeInspeccion.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
@@ -24,6 +25,7 @@ import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
